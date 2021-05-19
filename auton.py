4d09d92504885f3268c0f5d5a75c530935b662fa
Ken Ryan <Chen97me@gmail.com>
2021-05-18 18:15:12 -0700

    Update auton.py

--- auton.py
+++ auton.py
@@ -8,6 +8,7 @@ left_drive  = vexiq.Motor(3)
 right_drive = vexiq.Motor(4, True) # Reverse Polarity
 left_lift   = vexiq.Motor(5)
 right_lift  = vexiq.Motor(6, True)
+touch_led   = vexiq.TouchLed(9)
 
 import drivetrain
 dt          = drivetrain.Drivetrain(left_drive, right_drive, 200, 225)
@@ -15,32 +16,36 @@ dt          = drivetrain.Drivetrain(left_drive, right_drive, 200, 225)
 
 class liftTrain:
     def __init__(self,l,r):
-        this.left = l;
-        this.right = r;
+        self.left = l;
+        self.right = r;
     def lift_up(distance):
-        this.left.run(100,distance)
-        this.right.run(100,distance)
+        self.left.run(100,distance)
+        self.right.run(100,distance)
         sys.sleep(1)
     def lift_down(distance):
-        this.left.run(-100,distance)
-        this.right.run(-100,distance)
+        self.left.run(-100,distance)
+        self.right.run(-100,distance)
         sys.sleep(1)
 
 lt          = liftTrain(left_lift,right_lift)
 
 #Main function
-lt.lift_down(40)
-
-dt.drive_until(100,50)#forward
-dt.turn_until(100,-85)#turn to riser
-dt.drive_until(100,490)#drives forward and push it forward
+lt.lift_down(370);
+touch_led.brightness(80)
+while not touch_led.is_touch():
+    vexiq.lcd_write("Waiting for touch")
+touch_led.brightness(0)
+vexiq.lcd_write("")
+dt.drive_until(100,50)#robot a little forward
+dt.turn_until(100,-60)#turn right to riser
+dt.drive_until(100,335)#drives forward and push it forward
 dt.drive_until(-100,235)#drives back
-dt.turn_until(100,88)#turns to the left
-dt.drive_until(100,700)#foward
-dt.turn_until(100,-85)#turns to the right and faces pickel
-dt.drive_until(100,340)#push pickel into holy goaly
-dt.drive_until(-100,290)#run away from pickel in holy goaly
-dt.turn_until(100,85)#turn to the left
-dt.drive_until(100,630)#drive foward
-dt.turn_until(100,-85)#turn to riser
-dt.drive_until(100,315)#push in riser
+dt.turn_until(100,56)#turns to the left
+dt.drive_until(100,450)#foward
+dt.turn_until(100,-38)#turns to the right and faces pickel
+dt.drive_until(100,220)#push pickel into holy goaly
+dt.drive_until(-100,240)#run away from pickel in holy goaly rolly polly
+dt.turn_until(100,30)#turn to the left
+dt.drive_until(100,450)#drive foward
+dt.turn_until(100,-85)#turn to rise
+dt.drive_until(100,315)#push in ri
